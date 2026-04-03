48ae82eb0fd4b0cd4f4eb17bfc70a450a63c8f9b
Wale <onafesowale@gmail.com>
2026-04-03 11:10:59 +0100

    domain

---
+++ Airbnb.Domain/Apartments/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Airbnb.Domain.Apartments;
+
+public sealed record Address(
+    string Country,
+    string State,
+    string ZipCode,
+    string City,
+    string Street
+    );
---
+++ Airbnb.Domain/Apartments/Amenity.cs
@@ -0,0 +1,12 @@
+﻿namespace Airbnb.Domain.Apartments;
+
+public enum Amenity
+{
+    WiFi = 1,
+    AirConditioning = 2,
+    Gym = 3,
+    Pool = 4,
+    Spa = 5,
+    Parking = 6,
+    PetFriendly = 7
+}
